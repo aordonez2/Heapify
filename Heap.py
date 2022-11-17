8f04aae5f70b54feee533f5ab57d732873e0e9f2
Jett Midkiff <jdmidkiff2@catamount.wcu.edu>
2022-11-17 15:45:47 -0500

    Started work on Heap.py

--- Heap.py
+++ Heap.py
@@ -1,1 +1,18 @@
+class Heap:
+    #Temporary storage for the paths starting at tempPath[1]
+    tempPath = []
+    """
+    Reads inputFile given at the command line and places the contents of each line into the
+    path field found in each PathNode object. The order is the same as found in the text file.
+    Adds the PathNode object to tempPath starting at tempPath[1].
+    """
+    def read_paths(input_array):
+        if (len(input_array) == 0):
+            raise Exception ("Can't find input file or input file is empty")
+        else:
+            for i in input_array:
+                print ("Placeholder for loop printing out everything being passed to read_paths")
 
+
+    def build_complete_tree(index, parent):
+        return None
