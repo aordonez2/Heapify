bedac5d96f2f1f83c284c3dde57128f14a495a97
Jett Midkiff <jdmidkiff2@catamount.wcu.edu>
2022-11-17 14:55:44 -0500

    filled in some of PathNode class

--- PathNode.py
+++ PathNode.py
@@ -1,1 +1,21 @@
-
+class PathNode:
+    #An ArrayList of vertex IDs ordered by appearance in the path.
+    path = null
+    # A refrence to the left child node
+    right_child = null
+    #A refrence to the right child node
+    left_child = null
+    #A refrence to the parent node
+    parent_node = null
+    #Reference to the node directly to the left on the same tree level.
+    generation_left = null
+    #True if the node is last in the level
+    is_level_end = False
+    #True if the node is the right-most node in the last level.
+    is_last_node = False
+    
+    def __init__(self, path, right_child, left_child, parent_node):
+        self.path = path
+        self.right_child = right_child
+        self.left_child = left_child
+        self.parent_node = parent_node
