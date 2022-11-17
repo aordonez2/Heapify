84bc9733a8024b1eaf41d74af9ee8af29cd53831
aordonez2 <92597322+aordonez2@users.noreply.github.com>
2022-11-17 15:54:31 -0500

    Update Driver.py

--- Driver.py
+++ Driver.py
@@ -1,1 +1,12 @@
+class Driver:
+    try:
+        fielName = input("\nEnter Fiel Name:")
+        
+        with open(fielName) as f:
+            content_list = f.readlines()
 
+        # remove new line characters
+        content_list = [x.strip() for x in content_list]
+        print(content_list)
+    except:
+        print('\n\n\nYou have entered an invalid File Name.\n\n\n')
