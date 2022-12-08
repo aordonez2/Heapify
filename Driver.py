de18032b7a334cd53753b106ee503f0cead5e778
Jett Midkiff <jdmidkiff2@catamount.wcu.edu>
2022-12-08 11:29:34 -0500

    building build_complete_tree

--- Driver.py
+++ Driver.py
@@ -9,6 +9,8 @@ class Driver:
     def main():
         myHeap = Heap.Heap()
         myHeap.read_paths()
-        myHeap.build_tempPath_helper(myHeap.tempPath, content_list) 
+        myHeap.build_complete_tree(0, myHeap.tempPath[0], myHeap.tempPath)
+        print (myHeap.ll)
+        #myHeap.build_tempPath_helper(myHeap.tempPath, content_list) 
     if __name__=="__main__":
         main()
--- Heap.py
+++ Heap.py
@@ -1,10 +1,13 @@
 import PathNode
 import sys
+import Linked_List
 
 class Heap:
 
-    def __init__(self, path):
+    def __init__(self):
         self.tempPath = []
+        self.ll = Linked_List.LinkedList()
+
     """
     Reads inputFile given at the command line and places the contents of each line into the
     path field found in each PathNode object. The order is the same as found in the text file.
@@ -20,35 +23,41 @@ class Heap:
             newPathNode = PathNode.PathNode(temp)
             #add that item to tempPath
             self.tempPath.append(newPathNode)
+
+
+
+
+"""
+Recursively builds a complete binary tree. Places PathNode objects in tempPath ArrayList into a
+complete binary tree in order of appearance in the text file.
+
+@param index Index of the current node in tempPath.
+@param parent Parent of the current node.
+@return A reference to the node just placed in the tree.
+"""
+def build_complete_tree(index, parent, tempPath):
+    if parent == None:
+        return None
         
-    """
-    Recursively builds a complete binary tree. Places PathNode objects in tempPath ArrayList into a
-    complete binary tree in order of appearance in the text file.
+    parent.left = build_complete_tree(index, tempPath[index + 1], tempPath)#set left of parent node to value of pathnode at index + 1
+    parent.right = build_complete_tree(index, tempPath[index + 2], tempPath)#set right of parent node to value of pathnode at index + 2
+    
 
-    @param index Index of the current node in tempPath.
-    @param parent Parent of the current node.
-    @return A reference to the node just placed in the tree.
-    """
-    def build_complete_tree(index, parent):
-        #create a new tree
-        #for every element in tempPath call insert function
-        return None
+"""
+Recursive method that sets isLevelEnd.
+param root Root of the subtree.
+"""
+def set_level_end(root):
+    return None
 
-    """
-    Recursive method that sets isLevelEnd.
-    param root Root of the subtree.
-    """
-    def set_level_end(root):
-        return None
-
-    """
-    Recursive method that sets the "generation" link of PathNode objects from right-to-left.
-    generation is a term I use to indicate nodes on the same level (these may be siblings or
-    cousins)
-    @param root Root of the subtree.
-    """
-    def setGenerationLinks( root):
-        return None
+"""
+Recursive method that sets the "generation" link of PathNode objects from right-to-left.
+generation is a term I use to indicate nodes on the same level (these may be siblings or
+cousins)
+@param root Root of the subtree.
+"""
+def setGenerationLinks( root):
+    return None
 
     """
     Prints the node information from left-to-right at each level in the tree in the form specified
